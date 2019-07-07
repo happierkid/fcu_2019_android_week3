c5f9a95e1194c9cb1af0b87136e1e0024664c11e
wupojung <wupojung@gmai.com>
2019-07-07 12:39:52 +0800

    補充樂透的方法

--- app/src/main/java/edu/fcu/demo3/MainActivity.java
+++ app/src/main/java/edu/fcu/demo3/MainActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btnStart;
@@ -29,8 +32,37 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onClick(View view) {
-            tvResult.setText("測試...");
+            int[] result = GetLottery(1,49,6);
+            tvResult.setText(Arrays.toString(result));
         }
     };
+
+    private int[] GetLottery(int start, int end, int length) {
+        int[] result = new int[length];
+
+        //建立 ranger 資料 (樂透的球)
+        int temp_size = Math.abs(start - end);
+        //建立 亂數區
+        int[] temp = new int[temp_size];
+        for (int i = 0; i < temp_size; i++) {
+            temp[i] = start + i;
+        }
+        //撲克牌發牌擾亂程式
+        for(int i=temp_size-1;i>1;i--){
+            Random ran = new Random();
+            int random_index = ran.nextInt(i-1);
+            //swap random_index <--> last index
+            int swap_temp = temp[i-1];
+            temp[i-1] = temp[random_index];
+            temp[random_index] = swap_temp;
+        }
+
+        //取top length
+        for(int i=0;i<length;i++){
+            result[i] = temp[i];
+        }
+
+        return result;
+    }
 
 }
