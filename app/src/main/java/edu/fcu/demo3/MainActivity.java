880a0000d1a6b6754c16a4ac7200805aec8822d9
wupojung <wupojung@gmai.com>
2019-07-07 12:24:27 +0800

    新增 onclick 測試

--- app/src/main/java/edu/fcu/demo3/MainActivity.java
+++ app/src/main/java/edu/fcu/demo3/MainActivity.java
@@ -3,12 +3,30 @@ package edu.fcu.demo3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnStart ;
+    TextView tvResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //取得所有物件
+        btnStart= (Button)findViewById(R.id.btnStart);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        //設定物件的 內容 (on click event)
+        btnStart.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                tvResult.setText("測試...");
+            }
+        });
     }
 }
