46808af5627381f31afa631f012e837b9b540f16
wupojung <wupojung@gmai.com>
2019-07-07 12:25:32 +0800

    將方法抽出來獨立

--- app/src/main/java/edu/fcu/demo3/MainActivity.java
+++ app/src/main/java/edu/fcu/demo3/MainActivity.java
@@ -9,24 +9,28 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnStart ;
+    Button btnStart;
     TextView tvResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //取得所有物件
-        btnStart= (Button)findViewById(R.id.btnStart);
+        btnStart = (Button) findViewById(R.id.btnStart);
         tvResult = (TextView) findViewById(R.id.tvResult);
 
         //設定物件的 內容 (on click event)
-        btnStart.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view) {
-                tvResult.setText("測試...");
-            }
-        });
+        btnStart.setOnClickListener(btnStartOnClick);
     }
+
+    private View.OnClickListener btnStartOnClick = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            tvResult.setText("測試...");
+        }
+    };
+
 }
